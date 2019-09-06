c049e6ac6922cce691db1e6a5886e79ba14bcd30
Kenshin Abe <abe.kenshin@gmail.com>
2019-09-06 17:19:46 +0900

    add node num info option

--- chainer_chemistry/links/readout/general_readout.py
+++ chainer_chemistry/links/readout/general_readout.py
@@ -1,5 +1,5 @@
 import chainer
-from chainer import functions
+from chainer import functions, links
 
 
 class GeneralReadout(chainer.Link):
@@ -41,8 +41,15 @@ class GeneralReadout(chainer.Link):
 
 
 class ScatterGeneralReadout(chainer.Link):
-    def __init__(self, mode='sum', activation=None, **kwargs):
+    def __init__(self, mode='sum', activation=None, concat_n_info=False,
+                 add_n_info=False, **kwargs):
+        self.concat_n_info = concat_n_info
+        self.add_n_info = add_n_info
         super(ScatterGeneralReadout, self).__init__()
+        with self.init_scope():
+            if self.add_n_info:
+                self.linear1 = links.Linear(1, 1)
+                self.linear2 = links.Linear(1, 128)
         self.mode = mode
         self.activation = activation
 
@@ -58,4 +65,19 @@ class ScatterGeneralReadout(chainer.Link):
             y = functions.scatter_add(y, batch, h)
         else:
             raise ValueError('mode {} is not supported'.format(self.mode))
+
+        if self.concat_n_info:
+            n_nodes = self.xp.zeros(y.shape[0], dtype=self.xp.float32)
+            n_nodes = functions.scatter_add(n_nodes, batch,
+                                            self.xp.ones(batch.shape[0]))
+            y = functions.concat((y, n_nodes.reshape((-1, 1))))
+
+        if self.add_n_info:
+            n_nodes = self.xp.zeros(y.shape[0], dtype=self.xp.float32)
+            n_nodes = functions.scatter_add(n_nodes, batch,
+                                            self.xp.ones(batch.shape[0]))
+            n_nodes = n_nodes.reshape((-1, 1))
+            y = y + self.linear2(9 - n_nodes)
+            # y = y + self.linear2(-n_nodes)
+            # y = y + self.linear2(self.linear1(n_nodes))
         return y
